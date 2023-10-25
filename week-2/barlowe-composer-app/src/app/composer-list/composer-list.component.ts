e3e51111b280281b0c719837440f83fdbddba566
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2023-10-25 10:22:30 -0400

    Week 2

---
+++ week-2/barlowe-composer-app/src/app/composer-list/composer-list.component.ts
@@ -0,0 +1,39 @@
+// Name: Justin Barlowe
+// File: composer-list.component.ts
+// Description: TypeScript file for the composer list component
+// Date: 10/24/2023
+
+import { Component } from '@angular/core';
+
+// Define the Composer class
+export class Composer {
+  fullName: string;
+  genre: string;
+
+  constructor(fullName: string, genre: string) {
+    this.fullName = fullName;
+    this.genre = genre;
+  }
+}
+
+@Component({
+  selector: 'app-composer-list',
+  templateUrl: './composer-list.component.html',
+  styleUrls: ['./composer-list.component.css']
+})
+
+
+export class ComposerListComponent {
+  // Create an array of composers
+  composers: Array<Composer>;
+  // Constructor with instance of composers
+  constructor() {
+    this.composers = [
+      new Composer('Ludwig van Beethoven', 'Classical'),
+      new Composer('Johann Sebastian Bach', 'Baroque'),
+      new Composer('Wolfgang Amadeus Mozart', 'Classical'),
+      new Composer('Franz Schubert', 'Romantic'),
+      new Composer('George Frideric Handel', 'Baroque')
+    ];
+  }
+}
