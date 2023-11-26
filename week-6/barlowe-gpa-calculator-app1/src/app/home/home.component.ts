a40a5b4b40adc55b6ce65ae9544df4c8fc167987
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2023-11-26 16:45:05 -0500

    Assignment 6.3

---
+++ week-6/barlowe-gpa-calculator-app1/src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+// Name: Justin Barlowe
+// File: home.component.ts
+// Description: This is the home component for the GPA calculator app.
+// Date: 11/26/2023
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
